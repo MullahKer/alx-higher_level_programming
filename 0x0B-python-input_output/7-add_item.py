66723b9aa9a86fb166ec3b506c211e5c2f6f0e76
Mullah <104761945+MullahKer@users.noreply.github.com>
2022-09-28 12:55:18 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,44 @@
 #!/usr/bin/python3
-"""
-Script that adds all arguments to a Python list, and then saves them to a file
-"""
 
-from sys import argv
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+'''7-add_item.py
+    a script that adds all arguments to a Python list,
+    and then save them to a file
+'''
 
-filename = "add_item.json"
 
-try:
-    json_list = load_from_json_file(filename)
-except:
-    json_list = []
 
-for arg in argv[1:]:
-    json_list.append(arg)
+from os import path
 
-save_to_json_file(json_list, filename)
+import sys
+
+
+
+
+
+if __name__ == "__main__":
+
+    argv = sys.argv[1:]
+
+
+
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+
+    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+
+    file_path = "add_item.json"
+
+    if path.exists(file_path):
+
+        list_obj = load_from_json_file(file_path)
+
+    else:
+
+        list_obj = list()
+
+    
+
+    save_to_json_file(list_obj + argv, file_path)
+
+    print(end="")
